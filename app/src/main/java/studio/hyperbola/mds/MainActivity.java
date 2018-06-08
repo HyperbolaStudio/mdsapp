2f8b1791899bb432c5d442214289eea138335f39
Crindzebra <crs_16423@outlook.com>
2018-06-08 20:16:20 +0800

    1

---
+++ app/src/main/java/studio/hyperbola/mds/MainActivity.java
@@ -0,0 +1,138 @@
+package studio.hyperbola.mds;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.*;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private WebView webView;
+    private long exitTime = 0;
+    private String homeurl = "http://micokedatiservice.hyperbola.studio/";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "这是测试版应用 ver0.0.1_alpha", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //以下是webview代码
+        webView = (WebView)findViewById(R.id.wv);
+        webView.setWebViewClient(new WebViewClient() {
+            //设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setJavaScriptEnabled(true);  //设置WebView属性,运行执行js脚本
+        webView.loadUrl(homeurl);//载入首页
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (webView.canGoBack()) {
+            webView.goBack();
+        } else if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.refresh) {
+            webView.reload();
+            return true;
+        }else if(id==R.id.feedback){
+            // 必须明确使用mailto前缀来修饰邮件地址,如果使用
+            // intent.putExtra(Intent.EXTRA_EMAIL, email)，结果将匹配不到任何应用
+            Uri uri = Uri.parse("mailto:2082936582@qq.com");
+            String[] email = {"2082936582@qq.com"};
+            Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+            intent.putExtra(Intent.EXTRA_CC, email); // 抄送人
+            intent.putExtra(Intent.EXTRA_SUBJECT, "MDS-APP Feedback"); // 主题
+            intent.putExtra(Intent.EXTRA_TEXT, ""); // 正文
+            startActivity(Intent.createChooser(intent, "请选择邮件类应用"));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            webView.loadUrl(homeurl);
+        } else if (id == R.id.nav_today) {
+            webView.loadUrl(homeurl+"today/");
+        } else if (id == R.id.nav_testpaper) {
+            webView.loadUrl(homeurl+"testpaper/");
+        } else if (id == R.id.nav_article) {
+            webView.loadUrl(homeurl+"article/");
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(getApplicationContext(), "功能建设中，敬请期待", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_about) {
+            webView.loadUrl(homeurl+"about/");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
